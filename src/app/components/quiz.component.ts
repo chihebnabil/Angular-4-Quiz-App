d7ccd74b78390d8e78ecce939fcae07f29403de2
CHIHEB Nabil <CHIHEB Nabil>
2017-04-02 10:10:22 +0100

    formatting

--- src/app/components/quiz.component.ts
+++ src/app/components/quiz.component.ts
@@ -1,68 +1,68 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { QuizService } from '../services/quiz.service';
 import { ActivatedRoute } from '@angular/router';
-import { Router }  from '@angular/router';
-import {Observable} from 'rxjs/Rx';
-import {Question}  from "./Question"
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
+import { Question } from "./Question"
 
 @Component({
-    selector: 'quiz',
-    templateUrl: 'quiz.component.html',
-    providers : [QuizService]
+  selector: 'quiz',
+  templateUrl: 'quiz.component.html',
+  providers: [QuizService]
 })
 
 export class QuizComponent implements OnInit, OnDestroy {
 
-q:Question[];
-sub : any;
-id: number;
-cQuestion : number;
-score : number;
-qNumber : number;
+  q: Question[];
+  sub: any;
+  id: number;
+  cQuestion: number;
+  score: number;
+  qNumber: number;
 
 
-constructor ( private QuizService : QuizService , private route: ActivatedRoute,private router: Router){
-    
-}
+  constructor(private QuizService: QuizService, private route: ActivatedRoute, private router: Router) {
 
-nextQuestion( ){
-  
-   if(this.id == this.qNumber){
-     // when the last Question 
-     this.router.navigate(['/score' ]);
-   }else{
-     // navigate to next Question
-     console.log(this.q)
-     this.router.navigate(['/'+ (this.id +1) ]);
-   }
+  }
 
-}
-updateScore(){
+  nextQuestion() {
 
-}
+    if (this.id == this.qNumber) {
+      // when the last Question 
+      this.router.navigate(['/score']);
+    } else {
+      // navigate to next Question
+      console.log(this.q)
+      this.router.navigate(['/' + (this.id + 1)]);
+    }
 
-ngOnInit() {
- 
-     this.sub = this.route.params.subscribe(params => {
-       // (+) converts string 'id' to a number
-       // fetch the file and get next Question
+  }
+  updateScore() {
+
+  }
+
+  ngOnInit() {
+
+    this.sub = this.route.params.subscribe(params => {
+      // (+) converts string 'id' to a number
+      // fetch the file and get next Question
       this.id = +params['id'];
-      
-      if(localStorage.getItem('q') !== null){
+
+      if (localStorage.getItem('q') !== null) {
         var data = JSON.parse(localStorage.getItem('q'))
         console.log(data)
         this.q = data[this.id - 1]
-      }else{
-         this.QuizService.getQuestion(this.id)
-         var data = JSON.parse(localStorage.getItem('q'))
-         console.log(data)
-         this.q = data[this.id - 1]
+      } else {
+        this.QuizService.getQuestion(this.id)
+        var data = JSON.parse(localStorage.getItem('q'))
+        console.log(data)
+        this.q = data[this.id - 1]
       }
-      
+
     });
 
   }
-  
+
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,24 +1,24 @@
-import {Injectable} from "@angular/core";
-import {Http} from "@angular/http";
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
 import 'rxjs/add/operator/map'
-import {Observable} from 'rxjs/Rx';
-import {Question}  from "../components/Question"
+import { Observable } from 'rxjs/Rx';
+import { Question } from "../components/Question"
 
 @Injectable()
-export class QuizService{
-    constructor(private http : Http){
+export class QuizService {
+    constructor(private http: Http) {
     }
 
-    getQuestions(){
+    getQuestions() {
         return this.http.get("assets/q.json").map(res => res.json())
     }
     getQuestion(id) {
         return this.http.get("assets/q.json")
-        .map(res => res.json()).subscribe(
-          data => {
-            localStorage.setItem("q",JSON.stringify(data))
-          },
-          err => console.error(err)
-         )
+            .map(res => res.json()).subscribe(
+            data => {
+                localStorage.setItem("q", JSON.stringify(data))
+            },
+            err => console.error(err)
+            )
     }
 }
