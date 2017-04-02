9aa195f849b29c09ab96d9390f44ecdd0e13b492
CHIHEB Nabil <CHIHEB Nabil>
2017-04-02 12:32:13 +0100

    fix

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
 import 'rxjs/add/operator/map'
 import { Observable } from 'rxjs/Rx';
-import { Question } from "../components/Question"
 
 @Injectable()
 export class QuizService {
