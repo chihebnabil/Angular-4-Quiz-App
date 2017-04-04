17cb0a8c82ee0c5aca0f5017ff2b8a07d37f5597
CHIHEB Nabil <CHIHEB Nabil>
2017-04-04 12:45:51 +0100

    formatt doc

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,18 +1,15 @@
-import {ModuleWithProviders} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { QuizComponent } from './components/quiz.component';
-import { PageNotFoundComponent  } from './components/not-found.component';
-import { ScoreComponent  } from './components/score.component';
-import { HomeComponent  } from './components/home.component';
+import { PageNotFoundComponent } from './components/not-found.component';
+import { ScoreComponent } from './components/score.component';
+import { HomeComponent } from './components/home.component';
+
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent ,pathMatch: 'full' },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'score', component: ScoreComponent },
   { path: ':id', component: QuizComponent },
 ];
-<<<<<<< HEAD
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
-=======
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, { useHash: true });
->>>>>>> 868ad9f8617c18c155d2c3c91e176ff593503bc7
+
