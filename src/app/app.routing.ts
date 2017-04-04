2fe51db1abd0084dbbfef8971e636cef8b7120e7
Chiheb Nabil <chiheb.design@gmail.com>
2017-04-04 11:01:38 +0100

    Update app.routing.ts
    useHash

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -13,4 +13,4 @@ const appRoutes: Routes = [
 
 ];
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, { useHash: true });
